9d065cdd7b2954f5c8bdb3789710fdde35491c3e
Daisuke (yet another) Maki <maki.daisuke+gh@gmail.com>
2026-02-01 07:58:10 +0900

    fix(windows): implement robust symlink/junction detection using unsafe cast to access ReparseTag

--- issymlink_windows.go
+++ issymlink_windows.go
@@ -4,20 +4,42 @@ package main
 
 import (
 	"io/fs"
+	"sync"
 	"syscall"
 	"unsafe"
 )
 
-// isSymLink checks if the entry is a symbolic link.
-// Actually, this function checks if the entry is a reparse point (Symlink, Junction, etc.)
-// to ignore all kind of reparse points  using FILE_ATTRIBUTE_REPARSE_POINT
-// which correctly identifies Windows junction points.
-func isSymLink(entry fs.DirEntry) bool {
-	info, err := entry.Info()
-	if err != nil {
-		panic(err)
-	}
-	return unsafeCast(info.Sys()).FileAttributes&syscall.FILE_ATTRIBUTE_REPARSE_POINT != 0
+const (
+	IO_REPARSE_TAG_SYMLINK     = syscall.IO_REPARSE_TAG_SYMLINK
+	IO_REPARSE_TAG_MOUNT_POINT = 0xA0000003
+)
+
+// fileStat is copied from types_windows.go.
+// We use this struct to access the FileAttributes and ReparseTag.
+type fileStat struct {
+	name string
+
+	// from ByHandleFileInformation, Win32FileAttributeData, Win32finddata, and GetFileInformationByHandleEx
+	FileAttributes uint32
+	CreationTime   syscall.Filetime
+	LastAccessTime syscall.Filetime
+	LastWriteTime  syscall.Filetime
+	FileSizeHigh   uint32
+	FileSizeLow    uint32
+
+	// from Win32finddata and GetFileInformationByHandleEx
+	ReparseTag uint32
+
+	// what syscall.GetFileType returns
+	filetype uint32
+
+	// used to implement SameFile
+	sync.Mutex
+	path             string
+	vol              uint32
+	idxhi            uint32
+	idxlo            uint32
+	appendNameToPath bool
 }
 
 type eface struct {
@@ -25,13 +47,19 @@ type eface struct {
 	ptr unsafe.Pointer
 }
 
-// unsafeCast converts an interface to a pointer to its underlying data.
-// This is equivalent to the following code:
-//
-//	sys, ok := info.Sys().(*syscall.Win32FileAttributeData)
-//	return sys
-//
+// unsafeCast converts a FileInfo to a pointer to its underlying fileStat.
 // We bypass the type check to avoid the overhead of type assertion.
-func unsafeCast(s any) *syscall.Win32FileAttributeData {
-	return (*syscall.Win32FileAttributeData)((*eface)(unsafe.Pointer(&s)).ptr)
+func unsafeCast(fi fs.FileInfo) *fileStat {
+	return (*fileStat)((*eface)(unsafe.Pointer(&fi)).ptr)
+}
+
+// Contrary to its name, isSymLink returns true if the entry is a symbolic link, junction, or mount point.
+// All of them are treated as "symlinks" to prevent infinite loops and double counting.
+func isSymLink(entry fs.DirEntry) bool {
+	info, err := entry.Info()
+	if err != nil {
+		panic(err)
+	}
+	fs := unsafeCast(info)
+	return fs.FileAttributes&syscall.FILE_ATTRIBUTE_REPARSE_POINT != 0 && (fs.ReparseTag == IO_REPARSE_TAG_SYMLINK || fs.ReparseTag == IO_REPARSE_TAG_MOUNT_POINT)
 }
